bbdd2b5611b4f366504992e7ef406054a4e3b7ef
Soufianelk <Soufiane.loukili19@gmail.com>
2022-06-21 18:02:18 +0100

    added new files

---
+++ main.js
@@ -0,0 +1,42 @@
+/*==== SHOW MENU====*/
+const showMenu = (toggLeId, navId)=>{
+    const toggle = document.getElementById(toggLeId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+/*==== REMOVE MENU MOBILE ====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n=> n.addEventListener('click', linkAction))
+/*==== SCROLL SECTION ACTIVE LINK ====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll' ,scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = currentoffsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+
